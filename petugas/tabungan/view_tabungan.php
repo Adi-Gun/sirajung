14ed85a8e135c45162ed5defa19ffd09a32a8349
aadi.gunawan17 <you@example.com>
2023-08-04 19:07:00 +0700

    fitur report tabungan siswa

--- petugas/tabungan/view_tabungan.php
+++ petugas/tabungan/view_tabungan.php
@@ -72,63 +72,6 @@
 			<!-- /.box -->
 </section>
 
-<section class="content">
-	<div class="row">
-		<div class="col-md-12">
-			<!-- general form elements -->
-			<div class="box box-primary">
-				<div class="box-header with-border">
-					<h3 class="box-title">Cari Siswa</h3>
-					<div class="box-tools pull-right">
-						<button type="button" class="btn btn-box-tool" data-widget="collapse">
-							<i class="fa fa-minus"></i>
-						</button>
-						<button type="button" class="btn btn-box-tool" data-widget="remove">
-							<i class="fa fa-remove"></i>
-						</button>
-					</div>
-				</div>
-				<!-- /.box-header -->
-				<!-- form start -->
-				<form action="?page=data_tabungan" method="post" enctype="multipart/form-data">
-					<div class="box-body">
-
-						<div class="form-group">
-							<label>Siswa</label>
-							<select name="nis" id="nis" class="form-control select2" style="width: 100%;">
-								<option selected="selected">-- Pilih --</option>
-								<?php
-                        // ambil data dari database
-                        $query = "select * from tb_siswa where status='Aktif'";
-                        $hasil = mysqli_query($koneksi, $query);
-                        while ($row = mysqli_fetch_array($hasil)) {
-                        ?>
-								<option value="<?php echo $row['nis'] ?>">
-									<?php echo $row['nis'] ?>
-									-
-									<?php echo $row['nama_siswa'] ?>
-								</option>
-								<?php
-                        }
-                        ?>
-							</select required>
-						</div>
-
-						<div class="form-group">
-							<label>Saldo Tabungan</label>
-							<input type="text" name="saldo" id="saldo" class="form-control" placeholder="Saldo" readonly>
-						</div>
-
-					</div>
-					<!-- /.box-body -->
-
-					<div class="box-footer">
-						<input type="submit" name="Lihat" value="Lihat" class="btn btn-primary">
-					</div>
-				</form>
-			</div>
-			<!-- /.box -->
-</section>
 
 <script src="././bootstrap/lookup.js"></script>
 <script>
